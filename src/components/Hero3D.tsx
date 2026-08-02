387da74b60fb0e0d06d45d7a1af7f89d5a4c707e
UtkarshMudgal2802droid <utkarsh.mudgal123si@gmail.com>
2026-08-02 19:52:49 +0530

    Auto-commit: saved changes at 7:52:49 pm

--- src/components/Hero3D.tsx
+++ src/components/Hero3D.tsx
@@ -91,7 +91,7 @@ export default function Hero3D() {
         <PresentationControls 
           global 
           config={{ mass: 2, tension: 500 }} 
-          snap={{ mass: 4, tension: 1500 }} 
+          snap={true} 
           rotation={[0, 0, 0]} 
           polar={[-Math.PI / 3, Math.PI / 3]} 
           azimuth={[-Math.PI / 1.4, Math.PI / 2]}
